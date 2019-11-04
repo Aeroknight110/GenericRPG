85a8845a062ac86c949c024031d82a16f5fc7184
Tracey Smith <thebigorange0@gmail.com>
2019-11-04 15:23:05 -0600

    Updating References
    Chris and I ran into reference issues for the GenericRPG library. This update should fix them.

--- Implementation/GameLibrary/Mortal.cs
+++ Implementation/GameLibrary/Mortal.cs
@@ -75,7 +75,7 @@ namespace GameLibrary {
       Mana = MaxMana;
     }
     public void SimpleAttack(Mortal receiver) {
-      float baseDamage = Math.Abs((Str * 1.2f - receiver.Def) * (float)weapon.damMod);
+      float baseDamage = Math.Abs((Str * 1.2f - receiver.Def));
       float randMax = 1 + SIMPLEATTACK_RANDOM_AMT;
       float randMin = 1 - SIMPLEATTACK_RANDOM_AMT;
       float randMult = (float)(rand.NextDouble() * (randMax - randMin)) + randMin;
